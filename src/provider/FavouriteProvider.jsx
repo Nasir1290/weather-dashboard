04e8ccc5c29c3155be484ecaf6bcd0129bad6cbd
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-16 10:37:53 +0600

    modified some file

--- src/provider/FavouriteProvider.jsx
+++ src/provider/FavouriteProvider.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import useLocalStorage from "../components/hooks/useLocalStorage";
+import { favouriteContext } from "../context";
 
-export default function FavouriteProvider() {
+export default function FavouriteProvider({ children }) {
+  const { favourites, setFavourites } = useLocalStorage("favourites", []);
   return (
-    <div>
-      
-    </div>
-  )
+    <favouriteContext.Provider value={{ favourites }}>
+      {children}
+    </favouriteContext.Provider>
+  );
 }
