515e25c1602a0d987c5add622239b11e1cd590e1
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-16 10:17:03 +0600

    modified some file

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,3 +1,12 @@
-const useLocalStorage = () => {
-    
+import { useEffect, useState } from "react"
+
+const useLocalStorage = (storageKey,defaultValue) => {
+    const [value,setValue] = useState(JSON.parse(localStorage.getItem(storageKey))?? defaultValue);
+
+    useEffect(() => {
+        localStorage.setItem(storageKey, JSON.stringify(value));
+    },[value,storageKey]);
+
+    return {value,setValue}
 }
+export default useLocalStorage
