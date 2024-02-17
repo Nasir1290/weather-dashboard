c575dd464d5c3ada76302499be40aa790c0484f3
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-17 20:04:50 +0600

    modified some file

--- src/components/header/Favourite.jsx
+++ src/components/header/Favourite.jsx
@@ -5,7 +5,7 @@ import { favouriteContext } from "../../context";
 
 export default function Favourite({onShow}) {
   return (
-    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all"
+    <div className="p-2 hover:bg-black/30 text-white cursor-pointer flex gap-2 items-center rounded-md transition-all"
     onClick={onShow}
     >
       <img src={HeartImage} alt="favourite" />
